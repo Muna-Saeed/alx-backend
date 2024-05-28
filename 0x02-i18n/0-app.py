7980f5f3a828148cb61e934b1b5234355f7286de
Muna-Saeed <munasaeed6367@gmail.com>
2024-05-28 21:35:45 +0000

    0x02-i18n

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env python3
+"""
+Basic Flask app to display a welcome message.
+"""
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def index() -> str:
+    """
+    Route for the index page.
+    Returns:
+        The rendered HTML for the index page.
+    """
     return render_template('0-index.html')
 
 if __name__ == '__main__':
