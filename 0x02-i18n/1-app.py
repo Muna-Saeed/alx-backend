ab3c2605d1c52c0d282ea355d7ad6e60e1808de0
Muna-Saeed <munasaeed6367@gmail.com>
2024-05-28 21:51:28 +0000

    0x02-i18n

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -6,6 +6,7 @@ Basic Flask app with Babel for i18n.
 from flask import Flask, render_template
 from flask_babel import Babel
 
+
 class Config:
     """
     Config class for setting available languages and other settings.
@@ -14,10 +15,12 @@ class Config:
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
+
 app = Flask(__name__)
 app.config.from_object(Config)
 
 babel = Babel(app)
+
 
 @app.route('/')
 def index() -> str:
@@ -27,6 +30,7 @@ def index() -> str:
         The rendered HTML for the index page.
     """
     return render_template('1-index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
