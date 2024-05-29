c072a28d68b56e162c5da8819d505a924726be2c
Muna Saeed <munasaeed6367@gmail.com>
2024-05-29 10:37:28 -0700

    done

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -6,12 +6,14 @@ Basic Flask app with Babel for i18n, locale parameter, and mock login.
 from flask import Flask, render_template, request, g
 from flask_babel import Babel, _
 
+
 users = {
     1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
     2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
     3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
     4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
 }
+
 
 class Config:
     """
@@ -21,10 +23,12 @@ class Config:
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
+
 app = Flask(__name__)
 app.config.from_object(Config)
 
 babel = Babel(app)
+
 
 def get_user() -> dict:
     """
@@ -37,6 +41,7 @@ def get_user() -> dict:
         return users.get(int(user_id))
     return None
 
+
 @app.before_request
 def before_request():
     """
@@ -45,13 +50,15 @@ def before_request():
     """
     g.user = get_user()
 
+
 @babel.localeselector
 def get_locale() -> str:
     """
     Determine the best match for supported languages or use the locale
     parameter if present.
     Returns:
-        The best match for the user's preferred language or the specified locale.
+        The best match for the user's preferred language
+        or the specified locale.
     """
     locale = request.args.get('locale')
     if locale in app.config['LANGUAGES']:
@@ -59,6 +66,7 @@ def get_locale() -> str:
     if g.user and g.user['locale'] in app.config['LANGUAGES']:
         return g.user['locale']
     return request.accept_languages.best_match(app.config['LANGUAGES'])
+
 
 @app.route('/')
 def index() -> str:
@@ -69,6 +77,6 @@ def index() -> str:
     """
     return render_template('5-index.html')
 
+
 if __name__ == '__main__':
     app.run(debug=True)
-
