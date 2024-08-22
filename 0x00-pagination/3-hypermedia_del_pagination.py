21e197f05b653635fa9c2cefefd90d41e7b15cf0
Muna-Saeed <munasaeed6367@gmail.com>
2024-08-22 10:36:55 -0700

    done

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -85,8 +85,9 @@ class Server:
                 data.append(indexed_dataset[current_index])
             current_index += 1
 
-        next_index = current_index if current_index < len(indexed_dataset)
-        else None
+        next_index = (
+                current_index if current_index < len(indexed_dataset) else None
+                )
 
         return {
             "index": index,
