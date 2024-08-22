9ba454659026318cd06404238840cb56a8fafb7e
Muna-Saeed <124080410+Muna-Saeed@users.noreply.github.com>
2024-08-22 20:52:53 +0300

    Update 3-hypermedia_del_pagination.py

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -54,7 +54,7 @@ class Server:
         return self.__indexed_dataset
 
     def get_hyper_index(
-            self, index: int = None,
+            self, index: int = 0,
             page_size: int = 10
             ) -> Dict[str, Any]:
         """
